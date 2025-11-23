f641867081dd8d0b1fef3205791cdca5c3d3f4a8
DezzK <18627923+DezzK@users.noreply.github.com>
2025-11-23 14:04:28 +0500

    feat: add system app list from the Geely Monjaro head unit

--- app/src/main/java/dezz/stealth/AlwaysIgnoreAppResolver.java
+++ app/src/main/java/dezz/stealth/AlwaysIgnoreAppResolver.java
@@ -7,13 +7,25 @@ import java.util.Set;
 public final class AlwaysIgnoreAppResolver {
     private static final Set<String> SYSTEM_APP_LIST = new HashSet<>(List.of(
             "android",
-            "org.chromium.webview_shell"
+            "org.chromium.webview_shell",
+            "com.example.drtest",
+            "com.example.storagetest",
+            "com.example.ui.PASTestApp",
+            "log.debugtools.ecarx.logmanager",
+            "com.qti.snapdragon.qdcm_ff",
+            "net.easyconn",
+            "com.qti.diagservices"
     ));
 
     public static boolean alwaysIgnoreApp(String packageName, String stealthAppPackageName) {
         return packageName.equals(stealthAppPackageName) ||
+                packageName.startsWith("android.") ||
                 packageName.startsWith("com.android.") ||
                 packageName.startsWith("com.google.android.") ||
+                packageName.startsWith("ecarx.") ||
+                packageName.startsWith("com.ecarx.") ||
+                packageName.startsWith("com.qualcomm.") ||
+                packageName.startsWith("com.ts.") ||
                 SYSTEM_APP_LIST.contains(packageName);
     }
 }
